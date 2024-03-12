aa3b382d3358ba6375e71954c2e74af6b2240fd1
ElderGiovani <elder.dev@hotmail.com>
2024-03-12 14:40:39 -0300

    atualização_conversão_moedas

--- lib/app/models/views/home_view.dart
+++ lib/app/models/views/home_view.dart
@@ -1,7 +1,9 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+  const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,26 +17,46 @@ class HomeView extends StatelessWidget {
           child: Column(
             children: [
               Image.network(
-                'https://lh3.googleusercontent.com/mUuQy1PsIC2Mq1_ZGlUYP2pgY65qmMuNop5Ts1aKbWyMxNSrI8AEBdE-u26nnDDaAek',
+                'https://thumbs.dreamstime.com/z/%C3%ADcone-de-troca-moeda-isom%C3%A9trica-c%C3%A2mbio-em-dinheiro-do-d%C3%B3lar-para-shekel-moedas-ouro-com-setas-circundantes-s%C3%ADmbolo-d-sinal-273169736.jpg?w=768',
                 width: 200,
                 height: 200,
               ),
               SizedBox(
                 width: MediaQuery.of(context).size.width,
                 child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Expanded(
                         flex: 1,
-                        child: DropdownButton(items: [
-                          DropdownMenuItem(
-                            child: Text('Real'),
-                          ),
-                        ], onChanged: (value) {})),
+                        child: SizedBox(
+                          height: 56,
+                          child: DropdownButton(
+                              isExpanded: true,
+                              underline: Container(
+                                height: 1,
+                                color: Colors.amber,
+                              ),
+                              items: [
+                                DropdownMenuItem(
+                                  child: Text('Real'),
+                                ),
+                                //DropdownMenuItem(
+                                // child: Text('Dolar'),
+                                //),
+                              ],
+                              onChanged: (value) {}),
+                        )),
                     const SizedBox(width: 10),
                     const Expanded(
                       flex: 2,
                       child: TextField(
-                        decoration: InputDecoration(),
+                        decoration: InputDecoration(
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.amber),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.amber),
+                            )),
                       ),
                     ),
                   ],
